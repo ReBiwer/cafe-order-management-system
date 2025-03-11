4d5b534f8c74d09ab56ed1c20abd91cc90d7f94e
vladimir <vovka1998@gmail.com>
2025-03-11 14:31:57 +0300

    registered models in admin panel

--- cafe/orders/admin.py
+++ cafe/orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem, Dish
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "table_number", "status", "total_price", "created_at",
+    ]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "order", "dish_name", "price", "quantity", "status"
+    ]
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = [
+        "name", "available",
+    ]
