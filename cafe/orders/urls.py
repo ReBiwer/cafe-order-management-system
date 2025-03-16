64f10a79ed2a562152e8b9e61f6e84b2b0e6502e
vladimir <vovka1998@gmail.com>
2025-03-16 17:09:43 +0300

    added url for open Shift

--- cafe/orders/urls.py
+++ cafe/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
+from django.views.generic import TemplateView
 
-from .views import CreateOrder, ListOrders, DetailOrder, DeleteOrder, ChangeStatusOrder, APIOrder, SearchAPIOrder
+from .views import CreateOrder, ListOrders, DetailOrder, DeleteOrder, ChangeStatusOrder, APIOrder, SearchAPIOrder, OpenShift
 
 app_name = "orders"
 
@@ -10,6 +11,8 @@ router.register(r'', APIOrder, basename="api_order")
 
 urlpatterns = [
     path('', ListOrders.as_view(), name="list"),
+    path("open_shift/", OpenShift.as_view(), name="open_shift"),
+    path("shift_exist/", TemplateView.as_view(template_name="shift/exist_shift.html"), name="exist_shift"),
     path('create/', CreateOrder.as_view(), name="create"),
     path("search/", SearchAPIOrder.as_view(), name="search_order"),
     path('detail/<int:pk>/', DetailOrder.as_view(), name="detail"),
