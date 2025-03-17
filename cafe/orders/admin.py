99621baa76c677e3cbc48c407ee3a980cc8d1021
vladimir <vovka1998@gmail.com>
2025-03-17 10:54:27 +0300

    updated admin panel for Shift

--- cafe/orders/admin.py
+++ cafe/orders/admin.py
@@ -34,10 +34,19 @@ class DishAdmin(admin.ModelAdmin):
 class ShiftAdmin(admin.ModelAdmin):
     inlines = [OrderInline]
     list_display = [
-        "date_open", "active", "total_orders"
+        "date_open", "date_close", "total_orders", "active",
     ]
+    readonly_fields = ["revenue", "date_close", "active"]
 
     def total_orders(self, obj):
         return obj.orders.count()
+
+    def revenue(self, obj):
+        if not obj.active:
+            return obj.revenue
+
+    def date_close(self, obj):
+        if not obj.active:
+            return obj.date_close
 
     total_orders.short_description = "Всего заказов"
